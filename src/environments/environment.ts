d62101393bc28d1625d2265d71de3d40e38af87a
kabagemark <kabagemark@gmail.com>
2018-06-04 08:30:37 +0300

    first version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-
+'https://api.github.com/users/daneden?access_token= 6f3e047a5b170c15b2bbd99e08b54af9e3709d33';
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
